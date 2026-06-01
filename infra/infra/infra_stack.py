dd621fbc2a81aebe2d02a54d701e55dc2013587a
Shagnik <shagnikpaul.772@gmail.com>
2026-06-01 15:03:38 +0530

    chore: InfraStack defined and modified and deployed intial aws services using cdk

--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -1,19 +1,29 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    RemovalPolicy,
+    aws_s3 as s3,
+    aws_lambda as _lambda,
 )
 from constructs import Construct
 
+
 class InfraStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
+        
+        docs_bucket = s3.Bucket(
+            self,
+            "AwsenseDocsBucket",
+            bucket_name=f"awsense-docs-{self.account}-{self.region}",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "InfraQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        chatbot_lambda = _lambda.Function(
+            self,
+            "AwsenseLambda",
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler="handler.lambda_handler",
+            code=_lambda.Code.from_asset("../backend/src"),
+        )
