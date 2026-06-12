9b0909c554772570d0b504b85d1f53054a88a8cd
Shagnik <shagnikpaul.772@gmail.com>
2026-06-12 23:38:25 +0530

    fixed lint errors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import tailwindcssAnimate from "tailwindcss-animate"
+
 export default {
   darkMode: ["class"],
   content: [
@@ -77,5 +80,5 @@ export default {
       }
     }
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
